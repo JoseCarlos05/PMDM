dc398dd9b43514f3ab2e23dcdc2d37d13a3fbc86
JoseCarlos05 <jgomezbotana@safareyes.es>
2024-11-12 10:31:43 +0100

    Ejercicio 2 terminado.

--- Ejercicios_evaluables_2/Ejercicio2/app/src/main/java/com/example/ejercicio2/MainActivity.kt
+++ Ejercicios_evaluables_2/Ejercicio2/app/src/main/java/com/example/ejercicio2/MainActivity.kt
@@ -1,19 +1,22 @@
 package com.example.ejercicio2
 
 import android.annotation.SuppressLint
+import android.app.AlertDialog
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
 import android.widget.BaseAdapter
 import android.widget.GridView
+import android.widget.ImageButton
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity(){
+class MainActivity : AppCompatActivity() {
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,26 +29,47 @@ class MainActivity : AppCompatActivity(){
         }
 
         val gridView = findViewById<GridView>(R.id.gridView)
-        val imagenes = arrayOf(R.drawable.images)
+        val imagenes = arrayOf(R.drawable.images, R.drawable.hojas, R.drawable.paint, R.drawable.images, R.drawable.hojas, R.drawable.paint, R.drawable.images, R.drawable.hojas, R.drawable.paint)
 
         val adaptador = object : BaseAdapter() {
-
             override fun getCount(): Int {
-
+                return imagenes.size
             }
 
-            override fun getItem(p0: Int): Any {
-
+            override fun getItem(position: Int): Any {
+                return imagenes[position]
             }
 
-            override fun getItemId(p0: Int): Long {
-
+            override fun getItemId(position: Int): Long {
+                return position.toLong()
             }
 
-            override fun getView(p0: Int, p1: View?, p2: ViewGroup?): View {
+            override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+                val view = convertView ?: LayoutInflater.from(baseContext).inflate(R.layout.image_list, parent, false)
 
+                val imageButton = view.findViewById<ImageButton>(R.id.imagen)
+                imageButton.setImageResource(imagenes[position])
+
+                imageButton.setOnClickListener {
+
+                    val dialogBuilder = AlertDialog.Builder(parent?.context ?: return@setOnClickListener)
+                    dialogBuilder.setTitle("Imagen completa")
+
+                    val dialogView = LayoutInflater.from(parent.context).inflate(R.layout.image_full, null)
+
+                    val fullImageView = dialogView.findViewById<ImageView>(R.id.imagenCompleta)
+                    fullImageView.setImageResource(imagenes[position])
+
+                    dialogBuilder.setView(dialogView)
+                    dialogBuilder.setNegativeButton("Cerrar") { dialog, _ -> dialog.dismiss() }
+
+                    dialogBuilder.create().show()
+                }
+
+                return view
             }
-
         }
+
+        gridView.adapter = adaptador
     }
 }
